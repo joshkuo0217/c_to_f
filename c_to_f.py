a0fe852974c5050479abb5aad70ad8664ae3fe66
Chien Yu Kuo <josh820217@gmail.com>
2021-02-24 21:53:18 +0800

    add new file

---
+++ c_to_f.py
@@ -0,0 +1,4 @@
+Celsius = input('please enter a temperature in Celsius: ')
+Celsius = float(Celsius)
+Fahrenheit = Celsius * 9 / 5 + 32
+print('fianl translated temperature in Fahrenheit is', Fahrenheit)
